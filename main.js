3c3505fe0098029db30b284be76f84527dd4bc80
kevinromero <kevin.romero128@icloud.com>
2024-03-07 03:31:20 -0300

    Desafio 2

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const fs = require('fs').promises;
+const rutaArchivo = './products.json';
 class Product {
     static id = [];
     constructor(title, description, price, thumbnail, code, stock) {
@@ -11,11 +13,12 @@ class Product {
     }
 }
 class ProductManager {
-    constructor() {
+    constructor(path) {
+        this.path = path;
         this.products = [];
+        this.readProducts();
     }
-
-    addProduct(title, description, price, thumbnail, code, stock) {
+    async addProduct(title, description, price, thumbnail, code, stock) {
         if (!(title && description && price && thumbnail && code && stock)) {
             console.log('Todos los campos son obligatorios');
             return;
@@ -24,18 +27,15 @@ class ProductManager {
             console.log('Ya existe un Producto con el mismo codigo');
             return;
         };
-
         const newProduct = new Product (title, description, price, thumbnail, code, stock);
         this.products.push(newProduct);
+        await this.createProducts();
     };
-
     getProducts() {
         return this.products;
     };
-
     getProductById(id) {
         const getProductById = this.products.find(product => product.id === id);
-
         if (getProductById) {
             return getProductById;
         } else {
@@ -43,15 +43,50 @@ class ProductManager {
             return null;
         };
     };
-}
+    async createProducts() {
+        try {
+            await fs.writeFile(this.path, JSON.stringify(this.products, null, 2))
+        } catch (error) {
+            console.log('Error escribiendo productos:', error.message);
+        };
+    };
+    async readProducts() {
+        try {
+            const data = await fs.readFile(this.path, 'utf8');
+            this.products = JSON.parse(data);
+        } catch (error) {
+            console.log('Error leyendo los productos:', error.message);
+            this.products = [];
+        };
+    };
+    async updateProducts(id, updateProducts) {
+        const index = this.products.findIndex(product => product.id === id);
+        if (index !== -1) {
+            this.products[index] = {...this.products[index], ...updateProducts};
+            await this.createProducts();
+            console.log('Producto Actualizado!');
+        } else {
+            console.log('El Producto no pudo Actualizarse!');
+        };
+    };
+    async deleteProduct(id) {
+        const index = this.products.findIndex(product => product.id === id);
+        if (index !== -1) {
+            this.products.splice(index, 1);
+            await this.createProducts();
+            console.log('Producto Eliminado');
+        } else {
+            console.log('Producto no Encontrado');
+        };
+    };
 
-const prodMgr = new ProductManager;
+};
+
+const prodMgr = new ProductManager(rutaArchivo);
 
 prodMgr.addProduct('NIKE', 'ZAPATILLA NIKE', '$50,000', 'https://i.pinimg.com/564x/c3/57/d5/c357d503882fab5507f0376a50fa0bd9.jpg', '001', '30');
 
 prodMgr.addProduct('ADIDAS', 'ZAPATILLA ADIDAS', '$60,000', 'https://i.pinimg.com/originals/78/63/46/7863460405ffc7fa6195601bad60a879.jpg', '002', '10');
-
-prodMgr.addProduct('PUMA', 'ZAPATILLA PUMA', '$30,000', 'https://i.pinimg.com/originals/76/f6/01/76f6018113595149412c61e5dd78ccf0.jpg', '003', '50');
 
 prodMgr.addProduct('PUMA', 'ZAPATILLA PUMA', '$30,000', 'https://i.pinimg.com/originals/76/f6/01/76f6018113595149412c61e5dd78ccf0.jpg', '003', '50');
 
